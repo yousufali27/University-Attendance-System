ad9ce9fb0fb4cce7e27a98086e89775674c68991
yousufali27 <mdyousufali326049@gmail.com>
2026-08-28 00:53:00 +0600

    Clean up build.gradle.kts by removing comments and options
    Removed TODO comments and Kotlin compiler options from build.gradle.kts.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,10 +18,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = flutter.versionCode
@@ -30,16 +27,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
